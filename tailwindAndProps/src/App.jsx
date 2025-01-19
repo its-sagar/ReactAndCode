72db5d8b5ed7f317c80fb1ead1f734c50ea9623e
its-sagar <sagarsinghrajput.india@gmail.com>
2025-01-20 03:44:13 +0530

    Props and Tailwind

---
+++ tailwindAndProps/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card  from "./component/card"
+
+function App() {
+
+  return (
+    <>
+    <h1 className='bg-green-600'>Welcome To React</h1>
+    <Card username="Sagar"/>
+    <Card username="Sahil"/>
+    </>
+  )
+}
+
+export default App
